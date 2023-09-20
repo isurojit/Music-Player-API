3e2b25709e32550cbde94168efef2bca7d6c876e
isurojit <iamsurojit@hotmail.com>
2023-09-20 09:18:28 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const musicContainer = document.querySelector("#music-container");
+const playBtn = document.querySelector("#play");
+const preBtn = document.querySelector("#prev");
+const next = document.querySelector("#next");
+const audio = document.querySelector("#audio");
+const progress = document.querySelector("#progress");
+const progressContainer = document.querySelector("#progress-container");
